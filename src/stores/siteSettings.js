007db718428ae91c21dd6b9e8178cb6dcbeb7f39
cjg2772 <71551566+cjg2772@users.noreply.github.com>
2025-08-07 16:26:13 +0800

    Update siteSettings.js

--- src/stores/siteSettings.js
+++ src/stores/siteSettings.js
@@ -48,7 +48,7 @@ const useSiteSettingsStore = defineStore("siteSettings", {
       // 歌词部分
       lrcMousePause: false, // 鼠标移入歌词区域暂停滚动
       lyricsFontSize: 36, // 歌词大小
-      lyricsFont: "LXGW WenKai", // 歌词字体
+      lyricsFont: "PingFang SC", // 歌词字体
       lyricsBlur: true, // 歌词模糊
       lyricsBold: true, // 歌词加粗
       showYrc: true, // 是否显示逐字歌词
